ef43333f6fa13dd09d04b0c63317cc0b671624b9
Brian Koehler <brianbrifri@gmail.com>
2016-11-12 16:13:09 -0600

    just added 2 files

---
+++ Stopwatch/PageControl.swift
@@ -0,0 +1,21 @@
+//
+//  PageControl.swift
+//  Stopwatch
+//
+//  Created by vm mac on 11/12/16.
+//  Copyright © 2016 David Vaughn. All rights reserved.
+//
+
+import UIKit
+
+class PageControl: UIPageControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
